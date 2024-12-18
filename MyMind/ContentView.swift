c0d7ffa7c01a3f382f9995df446f41029e061a6d
Federica Villano <federica.villano00@gmail.com>
2024-12-18 16:57:27 +0100

    Initial Commit

---
+++ MyMind/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyMind
+//
+//  Created by Federica Villano on 18/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyMind/MyMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMindApp.swift
+//  MyMind
+//
+//  Created by Federica Villano on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MyMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
